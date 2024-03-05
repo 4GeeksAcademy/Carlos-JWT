0e27ac9e5118bc9c4269599d93bbed09031bcc57
Carlos Atanes Vences <carlos117g@gmail.com>
2024-03-05 20:31:34 +0000

    feat: backend

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +24,36 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/signup", methods=["POST"])
+def signup():
+    response_body = {}
+    data = request.json
+    user = User(email=data['email'],
+                password=data['password'],
+                is_active=True)
+    db.session.add(user)
+    db.session.commit()
+    response_body['message'] = 'User created successfully!'
+    return response_body, 200
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    response_body = {}
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+    if user and password == user.password:
+        access_token = create_access_token(identity=email)
+        return jsonify(access_token=access_token)
+    else:
+        response_body['message'] = "Tira pa atras moreno"
+    return response_body
+
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
