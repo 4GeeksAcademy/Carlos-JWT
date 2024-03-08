7c9ab87d0251d3e21d01a2fb163d6ae62681f826
Carlos Atanes Vences <carlos117g@gmail.com>
2024-03-08 16:27:09 +0000

    WIP: signup

--- src/front/js/pages/Signup.js
+++ src/front/js/pages/Signup.js
@@ -1,26 +1,79 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
+import { useNavigate } from "react-router-dom";
 
 export const Signup = () => {
+    /*
+    username=data['username'],
+    email=data['email'],
+    password=data['password'],
+    avatar_url=data['avatar_url'],
+    is_active=True)
+    */
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        avatar_url: ''
+    })
+
+    const handleInputChange = (event) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value })
+    };
+
+    const { store, actions } = useContext(Context)
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (confirmPassword === formData.password) {
+            actions.signup(formData)
+            alert('[PH] Registration Succesful!')
+        }
+
+    }
+
+    const handleCancel = () => {
+        navigate("/")
+    }
     return (
         <Container className="bg-dark-subtle border rounded-5 p-2 my-5" style={{ maxWidth: '50%' }}>
-            <Form>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="my-3" controlId="formBasicUsername">
+                    <Form.Label>Username *</Form.Label>
+                    <Form.Control type="text" placeholder="Username" name='username' value={formData.username} onChange={(e) => handleInputChange(e)} required />
+                </Form.Group>
+
                 <Form.Group className="my-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Label>Email address *</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" name='email' value={formData.email} onChange={(e) => handleInputChange(e)} required />
+                </Form.Group>
+
+                <Form.Group className="my-3" controlId="formBasicPassword">
+                    <Form.Label>Password *</Form.Label>
+                    <Form.Control type="password" placeholder="Password" name='password' value={formData.password} onChange={(e) => handleInputChange(e)} required />
+                </Form.Group>
+
+                <Form.Group className="my-3" controlId="formBasicConfirmPassword">
+                    <Form.Label>Confirm Password *</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" name='confirmPassword' value={confirmPassword} onChange={(e) => setConfirmPassword(e)} required />
+                </Form.Group>
+
+                <Form.Group className="my-3" controlId="formBasicImage">
+                    <Form.Label>Avatar URL</Form.Label>
+                    <Form.Control type="text" placeholder="http://www.url-image.com" name='avatar_url' value={formData.avatar_url} onChange={(e) => setAvatarUrl(e)} />
                     <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
+                        Leave blank for placeholder
                     </Form.Text>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
                 <Form.Group className="text-center mt-5">
-                    <Button className="mx-2 px-2" variant="danger" type="reset">
+                    <Button className="mx-2 px-2" variant="danger" type="button" onClick={handleCancel}>
                         Cancel
                     </Button>
                     <Button className="mx-2 px-2" variant="success" type="submit">
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,52 +2,45 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
+			results: null,
 			isLogin: false
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			APICall: async (url, options) => {
+				try {
+					const response = await fetch(url, options);
+					if (!response.ok) {
+						console.log('Error: ' + response.status, response.statusText);
+						return response.status;
+					}
+					return await response.json();
+				} catch (error) {
+					console.error('Error in fetch:', error);
+					return null;
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			
+			signin: async (data) => {
+				const options = {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(data),
+				}
+				return await getActions().APICall(process.env.BACKEND_URL + '/api/signin/', options);
+			},
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			signup: async (data) => {
+				const options = {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(data),
+				}
+				return await getActions().APICall(process.env.BACKEND_URL + '/api/signup/', options)
+			},
 		}
 	};
 };
